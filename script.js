dfbca8a9071de505150ce238aa94b3960daa86f0
megchen362 <77933062+megchen362@users.noreply.github.com>
2021-05-19 11:10:09 -0500

    Cleaned up class tags

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ let cur = [];
 mybutton = document.getElementById("button-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
